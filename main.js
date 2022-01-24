e6e6ce9c34cc52b9abc1926f6702c0afd4ff9a2e
marselinusharson <marselinus@undiksha.ac.id>
2022-01-24 18:37:32 +0800

    pisahkan method add rodo

--- main.js
+++ main.js
@@ -4,34 +4,31 @@ function clearfirstTodo() {
     todoListBody.removeChild(todoListBody.firstChild);
   }
 }
+
+function addTodoList(todo) {
+  const tr = document.createElement("tr");
+  const tdButton = document.createElement("td");
+  tr.appendChild(tdButton);
+
+  const buttonDone = document.createElement("input");
+  buttonDone.type = "button";
+  buttonDone.value = "Done";
+  tdButton.appendChild(buttonDone);
+  const tdTodo = document.createElement("td");
+  tdTodo.textContent = todo;
+  tr.appendChild(tdTodo);
+  const todoListBody = document.getElementById("todoListBody");
+  todoListBody.appendChild(tr);
+}
 function displayTodoList() {
   clearfirstTodo();
   for (let i = 0; i < todoList.length; i++) {
     const todo = todoList[i];
-    const tr = document.createElement("tr");
-    const tdButton = document.createElement("td");
-    tr.appendChild(tdButton);
-
-    const buttonDone = document.createElement("input");
-    buttonDone.type = "button";
-    buttonDone.value = "Done";
-    tdButton.appendChild(buttonDone);
-    const tdTodo = document.createElement("td");
-    tdTodo.textContent = todo;
-    tr.appendChild(tdTodo);
-    const todoListBody = document.getElementById("todoListBody");
-    todoListBody.appendChild(tr);
+    addTodoList(todo)
   }
 }
 
-const todoList = [
-  "Belajar HTML",
-  "Belajar PHP",
-  "Belajar JavaScript",
-  "Belajar CSS",
-  "Belajar Java",
-  "Belajar C++",
-];
+const todoList = ["Belajar HTML", "Belajar PHP", "Belajar JavaScript", "Belajar CSS", "Belajar Java", "Belajar C++"];
 document.forms["todoForm"].onsubmit = function (event) {
   event.preventDefault();
   const todo = document.forms["todoForm"]["todo"].value;
@@ -41,8 +38,11 @@ document.forms["todoForm"].onsubmit = function (event) {
 };
 
 const searchInput = document.getElementById("search");
-searchInput.onchange = function () {
-  console.log("On Change");
+searchInput.onckeyup = function () {
+  console.log("On key up");
+};
+searchInput.onkeydown = function () {
+  console.log("On key down");
 };
 
-displayTodoList()
+displayTodoList();
