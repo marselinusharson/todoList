5bf1a4cf0c94bc2296fd9115b023a4e7afefbe06
marselinusharson <marselinus@undiksha.ac.id>
2022-01-24 18:31:17 +0800

    button done

---
+++ main.js
@@ -0,0 +1,48 @@
+function clearfirstTodo() {
+  const todoListBody = document.getElementById("todoListBody");
+  while (todoListBody.firstChild) {
+    todoListBody.removeChild(todoListBody.firstChild);
+  }
+}
+function displayTodoList() {
+  clearfirstTodo();
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const tr = document.createElement("tr");
+    const tdButton = document.createElement("td");
+    tr.appendChild(tdButton);
+
+    const buttonDone = document.createElement("input");
+    buttonDone.type = "button";
+    buttonDone.value = "Done";
+    tdButton.appendChild(buttonDone);
+    const tdTodo = document.createElement("td");
+    tdTodo.textContent = todo;
+    tr.appendChild(tdTodo);
+    const todoListBody = document.getElementById("todoListBody");
+    todoListBody.appendChild(tr);
+  }
+}
+
+const todoList = [
+  "Belajar HTML",
+  "Belajar PHP",
+  "Belajar JavaScript",
+  "Belajar CSS",
+  "Belajar Java",
+  "Belajar C++",
+];
+document.forms["todoForm"].onsubmit = function (event) {
+  event.preventDefault();
+  const todo = document.forms["todoForm"]["todo"].value;
+  todoList.push(todo);
+  document.forms["todoForm"].reset();
+  displayTodoList();
+};
+
+const searchInput = document.getElementById("search");
+searchInput.onchange = function () {
+  console.log("On Change");
+};
+
+displayTodoList()
